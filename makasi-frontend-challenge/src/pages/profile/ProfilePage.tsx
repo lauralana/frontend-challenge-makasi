5fc51bde62054e9fc9c8517dd4dcd89f898a0369
lauramlana <laura.lana@lexartlabs.xyz>
2024-07-07 13:07:27 -0300

    feat: btn to collapse has wrong behavior

--- makasi-frontend-challenge/src/pages/profile/ProfilePage.tsx
+++ makasi-frontend-challenge/src/pages/profile/ProfilePage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { AppShell, Loader, Flex, Button } from "@mantine/core";
 import { useParams } from "react-router-dom";
+import { useDisclosure } from "@mantine/hooks";
+import { IconArrowBarRight, IconArrowBarLeft } from "@tabler/icons-react";
 import SideBar from "../../components/profile-components/SideBar";
 import RepoList from "../../components/profile-components/RepoList";
-import { useDisclosure } from '@mantine/hooks';
 
 const ProfilePage: React.FC = () => {
   const { username } = useParams<{ username: string }>();
+  const [opened, { toggle: toggleView }] = useDisclosure();
 
   if (!username) {
     return (
@@ -15,26 +17,36 @@ const ProfilePage: React.FC = () => {
       </Flex>
     );
   }
-  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
-  const [desktopOpened, { toggle: toggleDesktop }] =
-    useDisclosure(true);
 
   return (
     <AppShell
-    padding="md"
-    navbar={{
-      width:"30vw", 
-      breakpoint: "sm",
-      collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
-    }}
-  >
-    <AppShell.Navbar>
-      <SideBar username={username}/>
-    </AppShell.Navbar>
-    <AppShell.Main>
-      <RepoList username={username}/>
-    </AppShell.Main>
-  </AppShell>
+      padding="sm"
+      navbar={{
+        width: "30vw",
+        breakpoint: "sm",
+        collapsed: { mobile: !opened, desktop: opened },
+      }}
+    >
+      <AppShell.Navbar>
+        <SideBar username={username} />
+      </AppShell.Navbar>
+      <AppShell.Main>
+        <Flex>
+          <Button
+            variant="subtle"
+            onClick={toggleView}
+            style={{ position: "fixed", marginTop: "2vh", zIndex: 10 }}
+          >
+            {!opened ? (
+              <IconArrowBarLeft stroke={2} size={32} />
+            ) : (
+              <IconArrowBarRight stroke={2} size={32} />
+            )}
+          </Button>
+          <RepoList username={username} />
+        </Flex>
+      </AppShell.Main>
+    </AppShell>
   );
 };
 
