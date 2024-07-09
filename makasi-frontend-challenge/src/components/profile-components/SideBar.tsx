0a85aa412b8c6393c37dd8fca1ed7a4c4145787a
lauramlana <laura.lana@lexartlabs.xyz>
2024-07-09 20:17:12 -0300

    style: improve responsive css

--- makasi-frontend-challenge/src/components/profile-components/SideBar.tsx
+++ makasi-frontend-challenge/src/components/profile-components/SideBar.tsx
@@ -63,7 +63,7 @@ const SideBar: React.FC<SideBarProps> = ({ username }) => {
       <Flex
         justify="center"
         align="center"
-        style={{ height: "100vh", width: "100vw" }}
+        style={{ height: cellphone ? "20vh" : "100vh" }}
       >
         <Loader />
       </Flex>
@@ -101,89 +101,107 @@ const SideBar: React.FC<SideBarProps> = ({ username }) => {
 
   return (
     <Flex className={classes.col1}>
-      <img
-        src={user.avatar_url}
-        alt={`${user.login} avatar`}
-        className={classes.image}
-      />
-      <h2>{user.name}</h2>
-      <text>@{user.login}</text>
-      <text>{user.bio}</text>
-      <Flex align="center" gap="md">
-        <div className={classes.iconTextContainer}>
-          <IconUsers stroke={2} color="white" width={16} />
-          <text>{user.followers} followers</text>
-        </div>
-        <div className={classes.iconTextContainer}>
-          <IconHeart stroke={2} color="white" width={16} />
-          <text>{user.following} following</text>
-        </div>
-        {user.starredRepos.length > 0 ? (
-          <div className={classes.iconTextContainer}>
-            <IconStar stroke={2} color="white" width={16} />
-            <text>{user.starredRepos.length} stars</text>
-          </div>
-        ) : null}
+      <Flex
+        direction={cellphone ? "row" : "column"}
+        align={cellphone ? "flex-start" : "center"}
+        gap="md"
+      >
+        <img
+          src={user.avatar_url}
+          alt={`${user.login} avatar`}
+          className={classes.image}
+        />
+        <Flex direction="column" align={cellphone ? "flex-start" : "center"}>
+          <h2>{user.name}</h2>
+          <text>@{user.login}</text>
+          <text>{user.bio}</text>
+          <Flex align="center" gap="md">
+            <div className={classes.iconTextContainer}>
+              <IconUsers stroke={2} color="white" width={16} />
+              <text>{user.followers} followers</text>
+            </div>
+            <div className={classes.iconTextContainer}>
+              <IconHeart stroke={2} color="white" width={16} />
+              <text>{user.following} following</text>
+            </div>
+            {user.starredRepos.length >= 0 ? (
+              <div className={classes.iconTextContainer}>
+                <IconStar stroke={2} color="white" width={16} />
+                <text>{user.starredRepos.length} stars</text>
+              </div>
+            ) : null}
+          </Flex>
+        </Flex>
       </Flex>
-      <Flex className={classes.col1b}>
-        {user.company ? (
-          <div className={classes.iconTextContainerCol1b}>
-            <IconBuilding stroke={2} color="white" width={16} />
-            <text>{user.company}</text>
-          </div>
-        ) : null}
-        {user.location ? (
-          <div className={classes.iconTextContainerCol1b}>
-            <IconMapPin stroke={2} color="white" width={16} />
-            <text>{user.location}</text>
-          </div>
-        ) : null}
+      <Flex direction="column">
+        <Flex
+          className={classes.col1b}
+          direction={cellphone ? "row" : "column"}
+          gap={cellphone ? '15px' : '2px'}
+        >
+          {user.company ? (
+            <div className={classes.iconTextContainerCol1b}>
+              <IconBuilding stroke={2} color="white" width={16} />
+              <text>{user.company}</text>
+            </div>
+          ) : null}
+          {user.location ? (
+            <div className={classes.iconTextContainerCol1b}>
+              <IconMapPin stroke={2} color="white" width={16} />
+              <text>{user.location}</text>
+            </div>
+          ) : null}
+        </Flex>
         {user.email ? (
           <div className={classes.iconTextContainerCol1b}>
             <IconMail stroke={2} color="white" width={16} />
             <text>{user.email}</text>
           </div>
         ) : null}
-        {user.blog ? (
-          <div className={classes.iconTextContainerCol1b}>
-            <IconLink stroke={2} color="white" width={16} />
-            <Anchor
-              href={
-                user.blog.startsWith("http://") ||
-                user.blog.startsWith("https://")
-                  ? user.blog
-                  : `https://${user.blog}`
-              }
-              target="_blank"
-              underline="hover"
-              c="white"
-              style={{
-                fontSize: cellphone ? "10px" : "16px",
-                flexWrap: "wrap",
-              }}
-            >
-              {user.blog}
-            </Anchor>
-          </div>
-        ) : null}
-        {user.twitter_username ? (
-          <div className={classes.iconTextContainerCol1b}>
-            <IconBrandX stroke={2} color="white" width={16} />
-            <Anchor
-              href={`https://twitter.com/${user.twitter_username}`}
-              target="_blank"
-              underline="hover"
-              c="white"
-              style={{
-                fontSize: cellphone ? "10px" : "16px",
-                flexWrap: "wrap",
-              }}
-            >
-              {user.twitter_username}
-            </Anchor>
-          </div>
-        ) : null}
-      </Flex>
+        <Flex
+          className={classes.col1b}
+          direction={cellphone ? "row" : "column"}
+          gap={cellphone ? '15px' : '2px'}
+        >
+          {user.blog ? (
+            <div className={classes.iconTextContainerCol1b}>
+              <IconLink stroke={2} color="white" width={16} />
+              <Anchor
+                href={
+                  user.blog.startsWith("http://") ||
+                  user.blog.startsWith("https://")
+                    ? user.blog
+                    : `https://${user.blog}`
+                }
+                target="_blank"
+                underline="hover"
+                c="white"
+                style={{
+                  fontSize: cellphone ? "10px" : "16px",
+                  flexWrap: "wrap",
+                }}
+              >
+                {user.blog}
+              </Anchor>
+            </div>
+          ) : null}
+          {user.twitter_username ? (
+            <div className={classes.iconTextContainerCol1b}>
+              <IconBrandX stroke={2} color="white" width={16} />
+              <Anchor
+                href={`https://twitter.com/${user.twitter_username}`}
+                target="_blank"
+                underline="hover"
+                c="white"
+                style={{
+                  fontSize: cellphone ? "10px" : "16px",
+                  flexWrap: "wrap",
+                }}
+              >
+                {user.twitter_username}
+              </Anchor>
+            </div>
+          ) : null}
       <Button
         onClick={() => navigate(-1)}
         color="gray"
@@ -193,11 +211,15 @@ const SideBar: React.FC<SideBarProps> = ({ username }) => {
           fontSize: cellphone ? "8px" : "16px",
           fontStyle: "italic",
           backgroundColor: "gray",
-          marginTop: "16px",
+          marginTop: cellphone ? '2px' : "16px",
+          position:  cellphone ? 'absolute' : 'unset',
+          right: cellphone ? '15px' : '0'
         }}
       >
         Back
       </Button>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
